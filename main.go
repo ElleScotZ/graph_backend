3b22ea04962a2fbd0c01d809e45f81270bd196d9
Nikolett Galbicsek <nikolett.galbicsek@orfo.de>
2022-10-27 20:56:27 +0200

    new route

--- main.go
+++ main.go
@@ -339,5 +339,5 @@ func main() {
 	// Generating shoertest/longest paths
 	router.POST("/shortLong", getShortestLongestPath)
 
-	router.Run("https://graphbackend.herokuapp.com/")
+	router.Run("graphbackend.herokuapp.com/")
 }
