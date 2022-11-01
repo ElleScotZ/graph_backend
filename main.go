e017935d916ba85f6f4c3d80332fbfc0373520c7
Nikolett Galbicsek <nikolett.galbicsek@orfo.de>
2022-11-01 12:58:50 +0100

    url change

--- main.go
+++ main.go
@@ -316,7 +316,7 @@ func getShortestLongestPath(c *gin.Context) {
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://ellescotz.github.io/graph_frontend")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, OPTIONS, PATCH")
